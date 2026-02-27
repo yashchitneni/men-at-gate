9d50aee31620da42f5fb391f1e7dde21d5826e76
Yash Chitneni <yash.chitneni@gmail.com>
2026-02-27 12:46:14 -0600

    Add Vercel Speed Insights to Vite app

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import { initSentry } from "./lib/sentry";
 import App from "./App.tsx";
 import "./index.css";
 
 initSentry();
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <SpeedInsights />
+  </>,
+);
